e4e7625a994962c71e838e19dc9d584673721a1d
namrataojha <namrata.rohit22@gmail.com>
2016-03-09 07:06:42 -0800

    Speech to Text Implementation in Search Bar .

--- app/src/main/java/com/scu/smartgrocerytracker/items/ItemListingActivity.java
+++ app/src/main/java/com/scu/smartgrocerytracker/items/ItemListingActivity.java
@@ -1,11 +1,17 @@
 package com.scu.smartgrocerytracker.items;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -16,8 +22,35 @@ import com.scu.smartgrocerytracker.SmartGroceryDBHelper;
 import com.scu.smartgrocerytracker.constants.Constants;
 
 import java.util.List;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.util.ArrayList;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ItemListingActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1234;
     static Items itemClicked;
     SmartGroceryDBHelper dbHelper;
     String itemNameToAdd;
@@ -26,6 +59,13 @@ public class ItemListingActivity extends AppCompatActivity {
     int quantity;
     ItemListingAdpater itemAdapter;
     String itemPathh;
+    Button Start;
+    TextView Speech;
+    Dialog match_text_dialog;
+    ListView textlist;
+    ArrayList<String> matches_text;
+    SearchView searchv ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,18 +73,36 @@ public class ItemListingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ListView listView = (ListView) findViewById(R.id.listViewItem);
-        SearchView searchv = (SearchView) findViewById(R.id.searchView);
-
+        Start = (Button)findViewById(R.id.start_reg);
+        searchv = (SearchView) findViewById(R.id.searchView);
+        //Speech = (TextView)findViewById(R.id.speech);
         dbHelper = SmartGroceryDBHelper.getInstance(getApplicationContext());
         //Get  intent value
         Intent intent = getIntent();
-        TextView itemLabel = (TextView) findViewById(R.id.textViewLabelItem);
+      //  TextView itemLabel = (TextView) findViewById(R.id.textViewLabelItem);
         String categoryText = intent.getStringExtra(Constants.CATEGORY_NAME);
-        itemLabel.setText(categoryText);
+//        itemLabel.setText(categoryText);
         dbHelper = SmartGroceryDBHelper.getInstance(getApplicationContext());
         List<Items> item = dbHelper.getItemsByCategory(categoryText);
         itemAdapter = new ItemListingAdpater(this, item);
         listView.setAdapter(itemAdapter);
+
+
+
+        Start.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isConnected()) {
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    startActivityForResult(intent, REQUEST_CODE);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please Connect to Internet", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
 //
         searchv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -59,6 +117,7 @@ public class ItemListingActivity extends AppCompatActivity {
             }
         });
 
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -66,19 +125,60 @@ public class ItemListingActivity extends AppCompatActivity {
                 itemNameToAdd = itemClicked.getItemName();
                 categoryToAdd = itemClicked.getItemCategory();
                 itemID = itemClicked.getId();
-                itemPathh=itemClicked.getItemPath();
+                itemPathh = itemClicked.getItemPath();
                 quantity = 1;
-                dbHelper.insertItemInShoppingList(itemNameToAdd, categoryToAdd, quantity, itemID,itemPathh);
+                dbHelper.insertItemInShoppingList(itemNameToAdd, categoryToAdd, quantity, itemID, itemPathh);
 
                 String toast = "" + itemNameToAdd + " added to shopping List!!!";
                 Toast.makeText(ItemListingActivity.this, toast, Toast.LENGTH_LONG).show();
+                itemAdapter.getFilter().filter("");
+                searchv.setQuery("", false);
 
             }
 
         });
-
-
     }
+    public boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo net = cm.getActiveNetworkInfo();
+        if (net != null && net.isAvailable() && net.isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+            match_text_dialog = new Dialog(ItemListingActivity.this);
+            match_text_dialog.setContentView(R.layout.dialog_matches_frag);
+            match_text_dialog.setTitle("Select Matching Text");
+            textlist = (ListView) match_text_dialog.findViewById(R.id.list);
+            matches_text = data
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, matches_text);
+            textlist.setAdapter(adapter);
+            textlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+
+                   // Speech.setText("You have said " + matches_text.get(position));
+
+                    searchv.setQuery(matches_text.get(position), false);
+                    match_text_dialog.hide();
+                }
+            });
+            match_text_dialog.show();
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 
 
 //    @Override
